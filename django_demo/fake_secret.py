b41a5bfd062394168bd09644bdc50c7083554369
gowthamapandi-oss <gowthamapandi@seclogic.io>
2026-03-27 11:25:54 +0530

    Update fake_secret.py

--- django_demo/fake_secret.py
+++ django_demo/fake_secret.py
@@ -1,2 +1,3 @@
 OPENAI_API_KEY = "sk-1234567890abcdef"
 KEY = "sk-abcdefghijklmnop1234567890"   # hardcoded-openai-api-key
+KEY = "sk-ant-abcdefghijklmnopqrstuvwxyz1234"   #  hardcoded-anthropic-api-key
